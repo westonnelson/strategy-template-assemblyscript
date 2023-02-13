b823d6e903f20b13b49b8f934c945b8ad863544d
Brandon <brandonjmhunt@gmail.com>
2023-02-12 21:23:48 -0800

    feat: :fire: As conversion, simplified example

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const fs = require("fs");
 import * as AsBind from "as-bind/dist/as-bind.cjs.js";
 
 const imports = {
-  Logger: { // File which you are injecting
-    log(strPtr) {
-      console.log(strPtr)
-    }
-  }
+  env: {
+    abort(msg, file, line, column) {
+      console.error(
+        'abort called at ' + file + ' line:' + line + ':' + column,
+        'msg: ' + msg,
+      )
+    },
+  },
+  console: {
+    log: (msg) => {
+      console.log(msg)
+    },
+  },
 };
 
 const asBindInstance = AsBind.instantiateSync(fs.readFileSync(__dirname + "/build/optimized.wasm"), imports)
